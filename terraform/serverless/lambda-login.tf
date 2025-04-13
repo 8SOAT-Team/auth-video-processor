43c99437c4ee7d0fd3a12ab0a1c2f6230f2fc0cd
Darlei Randel <darley_rom@hotmail.com>
2025-04-13 13:02:17 -0300

    Fix signup lambda script

--- terraform/serverless/lambda-login.tf
+++ terraform/serverless/lambda-login.tf
@@ -12,10 +12,5 @@ resource "aws_lambda_function" "login" {
     }
   }
 
-  vpc_config {
-    subnet_ids         = data.terraform_remote_state.network.outputs.public_subnet_ids
-    security_group_ids = [data.terraform_remote_state.network.outputs.aws_security_group_id]
-  }
-
   depends_on = [aws_iam_role_policy_attachment.lambda_policy_attachment]
 }
