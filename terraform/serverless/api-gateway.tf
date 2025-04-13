360d103773d8a43d31e93210fb05e0289f1b7afe
Darlei Randel <darley_rom@hotmail.com>
2025-04-13 11:41:28 -0300

    Update api-gateway terraform file

--- terraform/serverless/api-gateway.tf
+++ terraform/serverless/api-gateway.tf
@@ -1,62 +1,59 @@
 resource "aws_apigatewayv2_api" "http_api" {
   name          = "http-api-auth-service"
   protocol_type = "HTTP"
-  description   = "HTTP API Gateway integrado com Lambda CPF Validator"
+  description   = "HTTP API Gateway para autenticação"
 }
 
-resource "aws_apigatewayv2_authorizer" "lambda_authorizer" {
-  api_id           = aws_apigatewayv2_api.http_api.id
-  name             = "cpf-authorizer"
-  authorizer_type  = "REQUEST"
-  authorizer_uri   = aws_lambda_function.login.invoke_arn
-  identity_sources = ["$request.header.Authorization"]
-  authorizer_payload_format_version = "2.0"
-
-  depends_on = [aws_lambda_function.login]
+resource "aws_apigatewayv2_integration" "login_lambda_integration" {
+  api_id                 = aws_apigatewayv2_api.http_api.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.login.invoke_arn
+  payload_format_version = "2.0"
 }
 
-resource "aws_lambda_permission" "http_api_gateway_authorizer_permission" {
-  statement_id  = "AllowHttpApiGatewayInvokeAuthorizer"
+resource "aws_apigatewayv2_integration" "signup_lambda_integration" {
+  api_id                 = aws_apigatewayv2_api.http_api.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.signup.invoke_arn
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "login_route" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "POST /login"
+  target    = "integrations/${aws_apigatewayv2_integration.login_lambda_integration.id}"
+  authorization_type = "NONE"
+}
+
+resource "aws_apigatewayv2_route" "signup_route" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "POST /signup"
+  target    = "integrations/${aws_apigatewayv2_integration.signup_lambda_integration.id}"
+  authorization_type = "NONE"
+}
+
+resource "aws_apigatewayv2_stage" "default_stage" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "$default"
+  auto_deploy = true
+  depends_on = [
+    aws_apigatewayv2_route.login_route,
+    aws_apigatewayv2_route.signup_route
+  ]
+}
+
+resource "aws_lambda_permission" "allow_api_gateway_login" {
+  statement_id  = "AllowInvokeLoginLambda"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.login.function_name
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*"
 }
 
-resource "aws_apigatewayv2_integration" "lambda_integration" {
-  api_id                 = aws_apigatewayv2_api.http_api.id
-  integration_type       = "AWS_PROXY"
-  integration_uri        = aws_lambda_function.login.invoke_arn
-  payload_format_version = "2.0"
-
-  depends_on = [aws_lambda_function.login]
-}
-
-resource "aws_apigatewayv2_route" "cpf_auth_route" {
-  api_id    = aws_apigatewayv2_api.http_api.id
-  route_key = "POST /auth"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
-  authorization_type = "CUSTOM"
-  authorizer_id      = aws_apigatewayv2_authorizer.lambda_authorizer.id
-
-  depends_on = [
-    aws_apigatewayv2_integration.lambda_integration,
-    aws_apigatewayv2_authorizer.lambda_authorizer
-  ]
-}
-
-resource "aws_apigatewayv2_stage" "dev_stage" {
-  api_id      = aws_apigatewayv2_api.http_api.id
-  name        = "dev"
-  auto_deploy = true
-
-  depends_on = [aws_apigatewayv2_route.cpf_auth_route]
-}
-
-resource "aws_lambda_permission" "http_api_gateway_invoke_lambda" {
-  statement_id  = "AllowHttpApiGatewayInvoke"
+resource "aws_lambda_permission" "allow_api_gateway_signup" {
+  statement_id  = "AllowInvokeSignupLambda"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.login.function_name
+  function_name = aws_lambda_function.signup.function_name
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*"
 }
