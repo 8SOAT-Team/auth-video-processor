16d77b17ba0b85b1586bc362b269941ee6440ff0
Darlei Randel <darley_rom@hotmail.com>
2025-04-13 12:16:10 -0300

    Fix python lib version

--- terraform/serverless/lambda-signup.tf
+++ terraform/serverless/lambda-signup.tf
@@ -6,14 +6,16 @@ resource "aws_lambda_function" "signup" {
   filename      = "lambda_signup.zip"
   source_code_hash = filebase64sha256("lambda_signup.zip")
 
+  layers = ["arn:aws:lambda:us-east-1:898466741470:layer:psycopg2-py39:1"]
+
   environment {
     variables = {
       COGNITO_USER_POOL_ID = aws_cognito_user_pool.user_pool.id
       COGNITO_CLIENT_ID    = aws_cognito_user_pool_client.user_pool_client.id
-      DB_HOST = data.terraform_remote_state.database.outputs.rds_endpoint
-      DB_NAME = data.terraform_remote_state.database.outputs.rds_db_name
-      DB_USER = data.terraform_remote_state.database.outputs.rds_username
-      DB_PASS = data.terraform_remote_state.database.outputs.rds_password
+      DB_HOST              = data.terraform_remote_state.database.outputs.rds_endpoint
+      DB_NAME              = data.terraform_remote_state.database.outputs.rds_db_name
+      DB_USER              = data.terraform_remote_state.database.outputs.rds_username
+      DB_PASS              = data.terraform_remote_state.database.outputs.rds_password
     }
   }
 
